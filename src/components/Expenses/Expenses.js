a90395e4ed6036fef1103634104a276e2457c5d9
Pablo <pablo.andre_alvarado@hotmail.com>
2022-04-02 22:11:11 -0600

    X

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import react, { useState } from "react";
+import { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,5 +1,3 @@
-import react from "react";
-
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css'
 
