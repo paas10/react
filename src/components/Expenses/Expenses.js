ff061cd1c0dd7ff9de81bcf3f0b6999009888f5a
Pablo <pablo.andre_alvarado@hotmail.com>
2022-04-02 22:43:04 -0600

    Apply filter to an rendered array

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,9 +14,13 @@ const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      { props.items.map(expense => 
-          <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-        )
+      { props.items
+          .filter(expense => 
+            (new Date(expense.date)).getFullYear().toString() === filteredYear
+          )
+          .map(expense => 
+            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+          )
       }
     </Card>
   );
