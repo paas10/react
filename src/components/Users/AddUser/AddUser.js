fd7f70f434ad94d8f579220039e11b8add5a3f69
Pablo <pablo.andre_alvarado@hotmail.com>
2022-04-09 19:24:12 -0600

    Ids for labels and inputs

--- src/components/Users/AddUser/AddUser.js
+++ src/components/Users/AddUser/AddUser.js
@@ -19,10 +19,10 @@ const AddUser = props => {
     <Card>
       <div className={styles.input}>
         <form onSubmit={submitHandler}>
-          <label>Username</label>
-          <input type='text' onChange={(event) => user.username = event.target.value}></input>
-          <label>Age (Years)</label>
-          <input type='number' onChange={(event) => user.age = event.target.value}></input>
+          <label htmlFor='username'>Username</label>
+          <input id='username' type='text' onChange={(event) => user.username = event.target.value}></input>
+          <label htmlFor='age'>Age (Years)</label>
+          <input id='age' type='number' onChange={(event) => user.age = event.target.value}></input>
           <Button text='Add User' type='submit' />
         </form>
       </div>
