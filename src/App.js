b9c015c62119a3166d77b4f666d873f2c9c6849f
Pablo <pablo.andre_alvarado@hotmail.com>
2022-04-09 19:27:24 -0600

    Page style moved to index.css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import styled from 'styled-components';
 import AddUser from './components/Users/AddUser/AddUser';
 import UsersList from './components/Users/UsersList/UsersList';
 
-const Div = styled.div`
-  height: 100vh;
-  background-color: #000;
-  padding: 40px;
-`;
-
 function App() {
   const [users, setUsers] = useState([]);
 
@@ -22,10 +16,10 @@ function App() {
   }
 
   return (
-    <Div>
+    <div>
       <AddUser handleSetUsers={handleSetUsers} />
       { users.length > 0 && <UsersList users={users} /> }
-    </Div>
+    </div>
   );
 }
 
