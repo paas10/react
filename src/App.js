785601cf82a014fe1950bf9eb0fd6db66ece0c5e
Pablo <pablo.andre_alvarado@hotmail.com>
2022-05-17 07:59:33 -0600

    Meals components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Header from './components/Layout/Header';
-
-import { DUMMY_MEALS } from './components/dummy-meals'
-
-
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <>
       <Header/>
+      <Meals />
     </>
   );
 }
