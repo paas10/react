60035038f20c8a4615186404127a8d7b2df208f2
Pablo <pablo.andre_alvarado@hotmail.com>
2022-05-30 16:25:20 -0600

    [Created]: Cart - Store, context, provider, dispatch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false)
@@ -15,11 +16,11 @@ function App() {
   }
 
   return (
-    <>
+    <CartProvider>
       <Header onShowCart={showCartHandler} />
       { cartIsShown && <Cart onCloseCart={hideCartHandler} /> }
       <Meals />
-    </>
+    </CartProvider>
   );
 }
 
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,21 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+  const cartCtx = useContext(CartContext)
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}><CartIcon /></span>
       <span>Your Cart</span>
-      <span className={styles.badge}>{props.cartNum}</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 }
