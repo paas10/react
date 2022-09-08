ffe7136e0cfbc0a463af18a76124f55c34b3a0ca
Pablo <pablo.andre_alvarado@hotmail.com>
2022-09-07 23:08:30 -0600

    Using async / await

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = () => {
-    fetch("https://swapi.dev/api/films")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        data = data.results.map((movie) => ({
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date,
-        }));
-        setMovies(data);
-      });
+  const fetchMoviesHandler = async () => {
+    const response = await fetch("https://swapi.dev/api/films");
+    let data = await response.json();
+    data = data.results.map((movie) => ({
+      id: movie.episode_id,
+      title: movie.title,
+      openingText: movie.opening_crawl,
+      releaseDate: movie.release_date,
+    }));
+    setMovies(data);
   };
 
   return (
